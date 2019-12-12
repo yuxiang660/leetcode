08bba6745d0f962feccbccf4b814629fed7026fb
yuxiang660 <yuxiang660@163.com>
2019-12-12 17:42:24 +0800

    To make the algorithm of tree travel more complete, add "travelBreadthFirst" and "travelDepthFirst".

--- code/94_144_145.Binary-Tree-Inorder_Preorder_Postorder-Traversal/main.cpp
+++ code/94_144_145.Binary-Tree-Inorder_Preorder_Postorder-Traversal/main.cpp
@@ -1,30 +1,62 @@
 #include "BinaryTree.h"
 #include <iostream>
+#include <queue>
 #include <stack>
 #include <vector>
 #include <boost/optional.hpp>
 
-std::vector<int> travelInorder(TreeNode* root)
+std::vector<int> travelBreadthFirst(TreeNode* root)
 {
-   std::vector<int> inorderNodes;
-   std::stack<TreeNode*> nodeStack;
+   std::vector<int> breadFirstNodes;
+   std::queue<TreeNode*> nodeQueue;
 
-   while (root != nullptr || !nodeStack.empty())
+   nodeQueue.push(root);
+
+   while (!nodeQueue.empty())
    {
-      if (root)
+      root = nodeQueue.front();
+      breadFirstNodes.push_back(root->val);
+      nodeQueue.pop();
+
+      if (root->left)
       {
-         nodeStack.push(root);
-         root = root->left;
+         nodeQueue.push(root->left);
       }
-      else
+
+      if (root->right)
       {
-         root = nodeStack.top()->right;
-         inorderNodes.push_back(nodeStack.top()->val);
-         nodeStack.pop();
+         nodeQueue.push(root->right);
       }
    }
 
-   return inorderNodes;
+   return breadFirstNodes;
+}
+
+std::vector<int> travelDepthFirst(TreeNode* root)
+{
+   std::vector<int> depthFirstNodes;
+   std::stack<TreeNode*> nodeStack;
+
+   nodeStack.push(root);
+
+   while (!nodeStack.empty())
+   {
+      root = nodeStack.top();
+      depthFirstNodes.push_back(root->val);
+      nodeStack.pop();
+
+      if (root->right)
+      {
+         nodeStack.push(root->right);
+      }
+
+      if (root->left)
+      {
+         nodeStack.push(root->left);
+      }
+   }
+
+   return depthFirstNodes;
 }
 
 std::vector<int> travelPreorder(TreeNode* root)
@@ -48,6 +80,29 @@ std::vector<int> travelPreorder(TreeNode* root)
    }
 
    return preorderNodes;
+}
+
+std::vector<int> travelInorder(TreeNode* root)
+{
+   std::vector<int> inorderNodes;
+   std::stack<TreeNode*> nodeStack;
+
+   while (root != nullptr || !nodeStack.empty())
+   {
+      if (root)
+      {
+         nodeStack.push(root);
+         root = root->left;
+      }
+      else
+      {
+         root = nodeStack.top()->right;
+         inorderNodes.push_back(nodeStack.top()->val);
+         nodeStack.pop();
+      }
+   }
+
+   return inorderNodes;
 }
 
 std::vector<int> travelPostorder(TreeNode* root)
@@ -94,14 +149,20 @@ void print(const std::vector<int> numbers)
 
 int main()
 {
-   BinaryTree tree({ 1, 2, 3, boost::none, boost::none, 4, 5 });
+   BinaryTree tree({ 1, 2, 3, boost::none, 4, 5 });
    tree.print();
 
-   std::cout << "Travel Inorder: ";
-   print(travelInorder(tree.get()));
+   std::cout << "Travel Breadth First: ";
+   print(travelBreadthFirst(tree.get()));
+
+   std::cout << "Travel Depth First: ";
+   print(travelDepthFirst(tree.get()));
 
    std::cout << "Travel Preorder: ";
    print(travelPreorder(tree.get()));
+
+   std::cout << "Travel Inorder: ";
+   print(travelInorder(tree.get()));
 
    std::cout << "Travel Postorder: ";
    print(travelPostorder(tree.get()));
