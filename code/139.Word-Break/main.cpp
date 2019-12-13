af97a565ede9060d9abb8f346d1956a56ded56f2
Ben <yuxiang660@163.com>
2019-12-13 15:44:05 +0800

    Update "139.Word-Break" solution
    1. Add a new DFS solution.
    2. Clean DP solution.

--- code/139.Word-Break/main.cpp
+++ code/139.Word-Break/main.cpp
@@ -1,44 +1,68 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
-bool hasTail(const std::string& word, const std::string& tail)
+bool isInWordDict
+(
+   const std::unordered_map<std::string, bool>& dict,
+   const std::string::const_iterator begin,
+   const std::string::const_iterator end
+)
 {
-   if (word.size() < tail.size()) return false;
-
-   size_t len = tail.size();
-   size_t pos = word.size() - tail.size();
-
-   return word.compare(pos, len, tail) == 0;
+   return (dict.count(std::string(begin, end)) > 0);
 }
 
-bool wordBreak(const std::string& words, const std::vector<std::string>& wordDict)
+bool wordBreak_DP(const std::string& words, const std::vector<std::string>& wordDict)
 {
-   std::vector<bool> breakTags(words.size());
-
-   for (size_t i = 0; i < words.size(); i++)
+   std::unordered_map<std::string, bool> dict;
+   for (auto&& word : wordDict)
    {
-      for (auto&& word : wordDict)
+      dict.emplace(word, true);
+   }
+
+   std::vector<bool> canBreakTags(words.size() + 1);
+
+   canBreakTags[0] = true;
+   for (int i = 1; i < canBreakTags.size(); i++)
+   {
+      for (int j = i - 1; j >= 0; j--)
       {
-         if (hasTail(std::string(words.begin(), words.begin() + i + 1), word))
+         if (canBreakTags[j] && isInWordDict(dict, words.begin() + j, words.begin() + i))
          {
-            size_t wordStartIndex = i + 1 - word.size();
-            if (wordStartIndex == 0 || breakTags[wordStartIndex - 1])
-            {
-               breakTags[i] = true;
-            }
+            canBreakTags[i] = true;
+            break;
          }
       }
    }
 
-   return breakTags.back();
+   return canBreakTags.back();
+}
+
+// @NOTE: The recursive solution will cause leetcode to report time limit error! But for the solution
+// of "140.Word-Break-II.md", this way has better performance than dynamic way, refer to "1440.Word-
+// Break-II.md".
+bool wordBreak_DFS(const std::string& subWords, const std::vector<std::string>& wordDict)
+{
+   if (subWords.empty()) return true;
+
+   for (const auto& word : wordDict)
+   {
+      if ((subWords.rfind(word, 0) == 0) && (wordBreak_DFS(subWords.substr(word.size()), wordDict))) return true;
+   }
+
+   return false;
 }
 
 int main()
 {
    std::cout << "139. Word Break" << std::endl;
 
-   std::cout << "Work Break: " << wordBreak("catsandog", {"cats", "dog", "san", "and", "cat"}) << std::endl;
+   const std::string words{ "applepenapple" };
+   const std::vector<std::string> wordDict{ "apple", "pen" };
+
+   std::cout << "Work Break(DP Solution): " << wordBreak_DP(words, wordDict) << std::endl;
+   std::cout << "Work Break(DFS Solution): " << wordBreak_DFS(words, wordDict) << std::endl;
 
    return 0;
 }
