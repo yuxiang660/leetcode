dfc4a5f0a97457d38f2dbbc12f2794ec0be9dd2f
yuxiang660 <yuxiang660@163.com>
2019-12-19 17:27:15 +0800

    Optimize "214. Shortest Palindrome" with KMP Algorithm.

--- code/214.Shortest-Palindrome/main.cpp
+++ code/214.Shortest-Palindrome/main.cpp
@@ -1,40 +1,9 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-
-bool isPalindrome(const std::string& s)
-{
-   for (int i = 0, j = s.size() - 1; j - i > 0; i++, j--)
-   {
-      if (j - i <= 0) return true;
-
-      if (s[i] != s[j]) return false;
-   }
-
-   return true;
-}
+#include <vector>
 
 std::string shortestPalindrome(const std::string& s)
-{
-   if (s.empty()) return "";
-
-   int i = s.size() - 1;
-   for (; i >= 0; i--)
-   {
-      if (isPalindrome(std::string(s.begin(), s.begin() + i + 1)))
-      {
-         break;
-      }
-   }
-
-   std::string res(s.begin() + i + 1, s.end());
-
-   std::reverse(res.begin(), res.end());
-
-   return res + s;
-}
-
-std::string shortestPalindrome2(const std::string& s)
 {
    if (s.empty()) return "";
 
@@ -42,7 +11,7 @@ std::string shortestPalindrome2(const std::string& s)
    std::reverse(rStr.begin(), rStr.end());
 
    int i = 0;
-   for (;i < rStr.size(); i++)
+   for (; i < rStr.size(); i++)
    {
       if (s.find(std::string(rStr.begin() + i, rStr.end())) == 0)
       {
@@ -53,12 +22,67 @@ std::string shortestPalindrome2(const std::string& s)
    return std::string(rStr.begin(), rStr.begin() + i) + s;
 }
 
+std::vector<int> getNext(const std::string& needle)
+{
+   std::vector<int> next(needle.size());
+
+   // If next[j] == k, it means that "needle[0] ~ needle[k - 1]" equals to "needle[j - k] ~ needle[j - 1]".
+   // To compute next[j + 1], we should compare needle[j] with another character. There are two cases:
+   // 1. if needle[j] == needle[k], then next[j + 1] = next[j] + 1.
+   // 2. if needle[j] != needle[k], then we should shrink "k" to "next[k]" until "needle[j] == needle[k]" or "k == -1".
+   next[0] = -1;
+   int k = next[0];
+   int j = 0; // start to compute next[j + 1]
+   while (j + 1 < static_cast<int>(needle.size()))
+   {
+      if (k == -1 || needle[k] == needle[j])
+      {
+         next[j + 1] = k + 1;
+         k = next[j + 1];
+         j++;
+      }
+      else
+      {
+         k = next[k];
+      }
+   }
+
+   return next;
+}
+
+std::string shortestPalindrome_KMP(const std::string& s)
+{
+   if (s.empty()) return "";
+
+   std::string rStr(s.rbegin(), s.rend());
+
+   auto next = getNext(s);
+
+   int len = static_cast<int>(s.size());
+   int j = 0;
+   int i = 0;
+   while (i < len && j < len)
+   {
+      if (j == -1 || rStr[i] == s[j])
+      {
+         j++;
+         i++;
+      }
+      else
+      {
+         j = next[j];
+      }
+   }
+
+   return std::string(rStr.begin(), rStr.begin() + (len - j)) + s;
+}
+
 int main()
 {
    std::cout << "214. Shortest Palindrome" << std::endl;
-   std::string s("aacecaaa");
+   std::string s("abcd");
    std::cout << "Shortest Palindrome: " << shortestPalindrome(s) << std::endl;
-   std::cout << "Shortest Palindrome(good): " << shortestPalindrome2(s) << std::endl;
+   std::cout << "Shortest Palindrome (KMP): " << shortestPalindrome_KMP(s) << std::endl;
 
    return 0;
 }
